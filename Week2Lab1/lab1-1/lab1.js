a8b07099118ca861907c95936b1e41c673e4a45e
Peiyu Han <hanpeiyu7755@gmail.com>
2024-09-10 11:07:03 -0400

    create

---
+++ Week2Lab1/lab1-1/lab1.js
@@ -0,0 +1,12 @@
+/*
+var firstGreeting = "Hello World";
+alert(firstGreeting);
+*/
+var Name = "Peiyu";
+var item1 = 23;
+var item2 = 365;
+var item3 = item1 * item2;
+var msg = " DAYS old… more or less.";
+//alert(item3);
+var msg2 = Name + " is " + item3 + msg;
+alert(msg2);
